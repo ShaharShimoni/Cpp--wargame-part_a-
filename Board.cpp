68b362543e3e9d0942d667c7cf1b53ad55e397de
shaharivka <57682267+shaharivka@users.noreply.github.com>
2020-05-26 22:40:18 +0300

    without move function

--- Board.cpp
+++ Board.cpp
@@ -18,19 +18,19 @@ Soldier* Board::operator[](std::pair<int,int> location) const{
 }
 
 void Board::move(uint player_number, std::pair<int,int> source, MoveDIR direction){//our actual move
-    Soldier* s=this->board[source.first][source.second];    //The soldier we need to move
-
-    if(s==NULL){     //There is no soldier in this place
-        cout<<"problem 1"<<endl;
-        //throw exception();
-    }
-    if(s->player_number!=player_number){    //The soldier doesnt belong to the player
-        cout<<"problem 2"<<endl;
-        //throw exception();
-    }
-   //actual move !!!! missing
-
-    s->attack_or_cure(this->board,source);  //22/5
+//    Soldier* s=this->board[source.first][source.second];    //The soldier we need to move
+//
+//    if(s==NULL){     //There is no soldier in this place
+//        cout<<"problem 1"<<endl;
+//        //throw exception();
+//    }
+//    if(s->player_number!=player_number){    //The soldier doesnt belong to the player
+//        cout<<"problem 2"<<endl;
+//        //throw exception();
+//    }
+//   //actual move !!!! missing
+//
+//    s->attack_or_cure(this->board,source);  //22/5
 
 }
 bool Board::has_soldiers(uint player_number) const{   //pass on all soldiers to see if they are of player_numer ?
